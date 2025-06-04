d6b9cd1f63f6588fcb31eca28d8fc0361e8db86d
Zhe-cyber <151831577+Zhe-cyber@users.noreply.github.com>
2025-06-04 21:57:04 +0800

    Update vite.config.js

--- cybersifu-frontend/vite.config.js
+++ cybersifu-frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-let process;
+
+//let process;
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/"
-})
+  //base: process.env.VITE_BASE_PATH || "/CyberSifu"
+  //base: process.env.VITE_BASE_PATH || "/CyberSifu"
+});
